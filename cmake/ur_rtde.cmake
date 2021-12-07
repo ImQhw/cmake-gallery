e6db855d1c6485defdda2f69761591f8d7d5742b
hongwei.qin <hongwei.qin@smartmore.com>
2021-12-07 15:32:15 +0800

    feat: fix bug of ur_rtde on windows

--- cmake/ur_rtde.cmake
+++ cmake/ur_rtde.cmake
@@ -1,5 +1,14 @@
-include(FetchContent)
+if(MSVC) # build on windows
 
+set(BOOST_ROOT C:/Users/qin_hw/Downloads/boost_1_77_0)
+set(BOOST_LIBRARYDIR C:/Users/qin_hw/Downloads/boost_1_77_0/libs)
+set(Boost_USE_STATIC_LIBS ON)
+
+endif()
+
+set(PYTHON_BINDINGS OFF CACHE INTERNAL "Turn off Python binding")
+
+include(FetchContent)
 FetchContent_Declare(
     ur_rtde 
     GIT_REPOSITORY https://gitlab.com/sdurobotics/ur_rtde.git
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <asio.hpp>
 #include <nlohmann/json.hpp>
+#include <ur_rtde/rtde_control_interface.h>
 
 int main(void)
 {
